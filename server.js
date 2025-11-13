397d8fe7222a92a62e8ca04630a3c92e3cd95c07
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-11-13 22:01:41 +0400

    build navbar

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const apiKey = `08eef91c5a865641cc85ae7b771d4002f78ef3cb`;
 import express from "express";
-import cors from "cors";
-import fetch from "node-fetch";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
-app.use(cors());
 
-app.get("/api/data", async (req, res) => {
-  const { query } = req.query;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
+app.get("/api/search", async (req, res) => {
   try {
-    const externalUrl = `https://www.giantbomb.com/api/search/?api_key=08eef91c5a865641cc85ae7b771d4002f78ef3cb&format=json&query=${encodeURIComponent(
+    const query = req.query.query || "";
+    const externalUrl = `https://www.giantbomb.com/api/search/?api_key=${apiKey}&format=json&query=${encodeURIComponent(
       query
     )}`;
-    const response = await fetch(externalUrl);
-    const data = await response.json();
-    res.json(data);
+    const response = await axios.get(externalUrl, {
+      headers: { "User-Agent": "MyGameApp/1.0" },
+    });
+    res.json(response.data);
   } catch (error) {
     console.error("Error fetching external API:", error);
     res.status(500).json({ error: "Failed to fetch external data" });
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,57 +1,96 @@
 import { useState, useEffect } from "react";
-import { InputText } from "primereact/inputtext";
+import temp from "../images/temp_logo.jpg";
 import { Link } from "@mui/material";
 
 const Navbar = () => {
-  const options = ["All", "Games", "Movies", "Tv series"];
-  const [dropItem, setDropItem] = useState(null);
   const [inputItem, setInputItem] = useState(null);
-  const [externalData, setExternalData] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [externalData, setExternalData] = useState([
+    { image: { icon_url: { temp } }, name: "test" },
+  ]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    if (!inputItem) return;
+    const delayDebounce = setTimeout(() => {
+      handleSearch();
+    }, 600);
+    return () => clearTimeout(delayDebounce);
+  }, [inputItem]);
   const handleSearch = async () => {
     try {
+      setLoading(true);
       const res = await fetch(
-        `http://localhost:3000/api/data?query=${InputText}`
+        `http://localhost:3000/api/search?query=${inputItem}`
       );
       const data = await res.json();
-      setExternalData(data);
+      setLoading(false);
+      setExternalData(data.results);
     } catch (error) {
       setError(error.massage);
-      setLoading(false);
     }
   };
-  console.log(externalData.results);
+  console.log(externalData);
   return (
-    <div className=" w-330 text-[white] h-20 flex items-center bg-[grey] rounded-lg px-2">
-      <p className="text-2xl">myv.</p>
-      <div className="relative flex items-center justify-end w-full">
-        <svg viewBox="0 0 24 24" fill="grey" className="size-4 left-6 relative">
-          <path
-            fillRule="evenodd"
-            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-            clipRule="evenodd"
+    <div
+      className={`w-330 text-[white] rounded-lg px-2 mt-2 bg-${
+        isFocused && "[grey]"
+      }`}
+    >
+      <div className="h-20 flex items-center">
+        <Link href="/" className="text-2xl">
+          myv.
+        </Link>
+        <div className="relative flex items-center justify-end w-full">
+          <svg
+            viewBox="0 0 24 24"
+            fill="grey"
+            className="size-4 left-6 relative"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="text"
+            value={inputItem || ""}
+            onChange={(e) => setInputItem(e.target.value)}
+            className={`${
+              isFocused ? "w-full" : "w-64"
+            } border-gray-300 transition-all duration-300 ease-in-out h-9 bg-[white] text-[black] rounded-lg pl-7 outline-0`}
+            placeholder="Search you game"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
           />
-        </svg>
-        <input
-          type="text"
-          value={inputItem || ""}
-          onChange={(e) => setInputItem(e.target.value)}
-          className={`${
-            isFocused ? "w-full" : "w-64"
-          } border-gray-300 transition-all duration-300 ease-in-out h-9 bg-[white] text-[black] rounded-lg pl-7 outline-0`}
-          placeholder="Search you game"
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-        <button className="pl-5" onClick={handleSearch}>
-          find
-        </button>
-        <div className="w-50">
-          <Link className="pl-5">Log in</Link>
-          <Link className="pl-5">Sign Up</Link>
+          <div className="w-50">
+            <Link href="/log_in" className="pl-5">
+              Log in
+            </Link>
+            <Link href="sign_up" className="pl-5">
+              Sign Up
+            </Link>
+          </div>
         </div>
+      </div>
+      <div className={`${!isFocused && "hidden"}`}>
+        {loading && <p>Loading...</p>}
+        {externalData.map((item, index) => (
+          <div key={index} className={`flex gap-3`}>
+            <div className="pb-1">
+              <img
+                className="h-full w-20 rounded-lg"
+                src={item.image.icon_url}
+              />
+            </div>
+            <div>
+              <h1>{item.name}</h1>
+              <p>{item.deck}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
